13b5f9d65a7adc741e70e12dbbf4b34716471ceb
AyahBot-Dev <the.official.ayahbot@gmail.com>
2022-05-18 06:32:55 +0600

    [fix]: changed .js to .cjs for pm2 ecosystem file

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ build:
 	yarn tsc
 	cp package.json build
 	cp .env build
-	cp ecosystem.config.js build
+	cp ecosystem.config.cjs build
 	cd build
 	yarn install --production
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf build ||:
 yarn tsc
 cp package.json build
 cp .env build
-cp ecosystem.config.js build
+cp ecosystem.config.cjs build
 cd build
 yarn install --production
 exit 0
