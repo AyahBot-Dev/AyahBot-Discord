c3ae58f35393266422feebf5b2c5cf8562f05bd7
AyahBot-Dev <the.official.ayahbot@gmail.com>
2022-07-07 08:09:45 +0600

    [extra]: added actions for checking new translations

---
+++ tools/check_new_translations.ts
@@ -0,0 +1,46 @@
+import axios from "../src/lib/axiosInstance";
+import { transport } from "../src/lib/utils";
+import { translations } from "../src/lib/classes/Ayah";
+
+const notifyUpdate = async (text: string): Promise<boolean> => {
+  const message = {
+    from: `AyahBot Mailer <${process.env.SENDER_MAIL}>`,
+    to: process.env.RECEIVER_MAIL,
+    subject: "Translations array needs an update",
+    text: text,
+    html: text,
+  };
+  return await transport
+    .sendMail(message)
+    .then(async () => {
+      return true;
+    })
+    .catch(() => process.exit(1));
+};
+
+const translationsArr = Object.values(translations);
+
+const url = "https://api.quran.com/api/v4/resources/translations";
+
+const translationsdTC = (await axios.get(url)).data?.translations as Array<
+  Record<string, any>
+>;
+
+translationsdTC.sort((a, b) => a.id - b.id);
+
+// check the length first:
+if (translationsdTC.length != translationsArr.length) {
+  await notifyUpdate("Length doesn't match");
+  await new Promise((r) => setTimeout(r, 3000));
+}
+
+// then iterate if all ok
+await translationsdTC.forEach(async (v, i) => {
+  if (v.id != translationsArr[i] || !translationsArr[i]) {
+    await notifyUpdate(
+      `So, in index ${i}, translations list (online) has ${v.id}, but we have ${translationsArr[i]}`
+    );
+    await new Promise((r) => setTimeout(r, 3000));
+  }
+  i == translationsdTC.length - 1 ? process.exit(0) : null;
+});
