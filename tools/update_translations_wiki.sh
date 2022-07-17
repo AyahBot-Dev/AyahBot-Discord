0e2986005161144f1d213b154804e7ce7eaef0fe
AyahBot-Dev <the.official.ayahbot@gmail.com>
2022-07-17 18:44:55 +0600

    [extra]: testing gh action

--- tools/update_translations_wiki.sh
+++ tools/update_translations_wiki.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -eu
 
-REPO_PATH=${PWD}
+TEMP_REPO_DIR="wiki_action_$GITHUB_REPOSITORY$GITHUB_SHA"
+TEMP_WIKI_DIR="temp_wiki_$GITHUB_SHA"
 
+echo "Cloning repo https://github.com/$GITHUB_REPOSITORY"
+git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY" "$TEMP_REPO_DIR"
+
+echo "Creating updated translations file"
+cd $TEMP_REPO_DIR
 yarn install --frozen-lockfile
 yarn tsm -r dotenv/config tools/update_translations_wiki.ts
 echo "Done creating translations markdown file"
@@ -10,17 +16,17 @@ echo "Done creating translations markdown file"
 #Clone wiki repo
 echo "Cloning wiki repo https://github.com/$GITHUB_REPOSITORY.wiki.git in parent folder"
 cd ../
-git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "wiki"
+git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_WIKI_DIR"
 #Get commit details
 author=`git log -1 --format="%an"`
 email=`git log -1 --format="%ae"`
 message=`git log -1 --format="%s"`
 
-echo "Copying edited wiki"
-cp "$REPO_PATH/Translations.md" "wiki/"
+echo "Copying edited translations file"
+cp "$TEMP_REPO_DIR/Translations.md" "$TEMP_WIKI_DIR/"
 
 echo "Checking if wiki has changes"
-cd "wiki"
+cd "$TEMP_WIKI_DIR"
 git config --local user.email "$email"
 git config --local user.name "$author" 
 git add .
