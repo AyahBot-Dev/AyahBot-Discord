31f59303583efe432ae395eb6e363b29aded6643
AyahBot-Dev <the.official.ayahbot@gmail.com>
2022-07-17 06:49:37 +0600

    [extra]: testing gh action

--- tools/update_translations_wiki.sh
+++ tools/update_translations_wiki.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 set -eu
 
-CURR_PATH=`basename "$PWD"`
-
 TEMP_WIKI_DIR="temp_wiki_$GITHUB_SHA"
 WIKI_DIR='wiki'
 
-cd ../
 #Clone wiki repo
 echo "Cloning wiki repo https://github.com/$GITHUB_REPOSITORY.wiki.git"
-cd CURR_PATH
 git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_WIKI_DIR"
 
 #Get commit details
