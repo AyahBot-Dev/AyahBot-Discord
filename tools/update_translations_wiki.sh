90efe987ef775c99a7e64cfffefccb5d8dab3863
AyahBot-Dev <the.official.ayahbot@gmail.com>
2022-07-17 20:05:44 +0600

    [extra]: testing gh action

--- tools/update_translations_wiki.sh
+++ tools/update_translations_wiki.sh
@@ -9,7 +9,7 @@ git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY" "$TEMP
 
 echo "Creating updated translations file"
 cd $TEMP_REPO_DIR
-ls tools
+git checkout dev
 yarn install --frozen-lockfile
 yarn tsm -r dotenv/config $TEMP_REPO_DIR/tools/update_translations_wiki.ts
 echo "Done creating translations markdown file"
